ea99dac959c94bc94f4746feda343097b6cd790e
khannaavik <avik1999.ak@gmail.com>
2026-01-04 13:39:48 +0530

    Apply initial database schema

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,11 +1,9 @@
-import { defineConfig, env } from 'prisma/config';
+import "dotenv/config";
+import { defineConfig } from "@prisma/config";
 
 export default defineConfig({
-  schema: 'prisma/schema.prisma',
-  migrations: {
-    path: 'prisma/migrations',
-  },
   datasource: {
-    url: env('DATABASE_URL'),
+    provider: "postgresql",
+    url: process.env.DATABASE_URL!,
   },
 });
